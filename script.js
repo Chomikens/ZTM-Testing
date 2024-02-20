a1577770dfdc032b314712916268b0b634f62904
d.kantorowicz@odo24.pl <kotorozec.code@gmail.com>
2024-02-20 11:38:01 +0100

    Simple function to search

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ const database = [
     'catpics.com',
     'myfav.com',
 ]
+
+const databaseSearch = (userInput) => {
+    const matches = database.filter(web =>{
+    return web.toLowerCase().includes(userInput.toLowerCase())
+    })
+    return matches.length > 3 ? matches.slice(0, 3) : matches
+}
+
+
